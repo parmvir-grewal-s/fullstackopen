ee9cab5d2f2ffa5baeb602ea3967be8dbbb758de
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-06-26 16:34:41 +0100

    Part 2 submission

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,33 +1,92 @@
-import { useState } from 'react'
-import axios from "axios";
+import { useEffect, useState } from 'react'
+import countryService from './services/countries'
 
-const Search = (props) => {
+const DetailedCountry = (country) => {
+  console.log(country)
   return (
-      <input onChange={props.handleSearch}></input>
+    <div>
+      <h2>{country.name.common}</h2>
+      capital {country.capital}
+      <br></br>
+      area {country.area}
+      <br></br>
+      <br></br>
+      <b>languages</b>
+      <br></br>
+      {Object.values(country.languages).map(language => {
+        return (
+          <li key={language}>{language}</li>
+        )
+      })}
+      <img src={country.flags.png}></img>
+    </div>
   )
 }
 
-const Warning = ({message}) => {
+const Countries = (props) => {
+  var countries = props.countries
+  if (countries.length < 10) {
+    if (countries.length == 1) {
+      return DetailedCountry(countries[0])
+    }
+    return (
+      countries.map(country => 
+      <li key={country.name.common}>{country.name.common} <button onClick={() => {
+        return DetailedCountry(country)
+      }}>show</button></li>)
+    )
+  }
+}
+
+const Search = (props) => {
+  return (
+      <input onChange={props.handleFilter}></input>
+  )
+}
+
+const Warning = (props) => {
+  if (props.countries > 10)
   return (
     <div>
-      {message}
+      {props.message}
     </div>
   )
 }
 
 function App() {
-  const [value, setValue] = useState('')
   const [message, setMessage] = useState('')
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [country, setCountry] = useState({})
 
-  const handleSearch = (event) => {
-    event.preventDefault()
-    setValue(event.target.value)
+  useEffect(() => {
+    countryService.getAll()
+    .then(initialCountries => {
+      setCountries(initialCountries)
+        })
+  }, [])
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value.toLowerCase())
+    if (filteredCountries.length > 10) {
+      setMessage('Too many matches, please specify another filter')
+    }
+    else {
+      setMessage('')
+    }
+  }
+
+  const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(filter))
+
+  const handleShow = (country) => {
+    setCountry(country)
   }
 
   return (
     <div>
-      find countries <Search handleSearch={handleSearch}/>
-      <Warning message={message} />
+      find countries <Search handleFilter={handleFilter}/>
+      <Warning message={message} countries={filteredCountries.length}/>
+      <Countries countries={filteredCountries} handleShow={handleShow}/>
     </div>
   )
 }
