2be183a026b805c559a451e7e0d163478a7de7c1
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-07-01 11:39:37 +0100

    made baseURL a relative path due to front end and backend now being from the same source

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 const cors = require('cors')
