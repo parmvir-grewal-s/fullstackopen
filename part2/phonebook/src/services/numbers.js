1071ec0044c82cd219afe21ff370cd19cffda7e0
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-07-01 11:29:26 +0100

    creating production front end for back end

--- part2/phonebook/src/services/numbers.js
+++ part2/phonebook/src/services/numbers.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'https://phonebookfullstack-y11u.onrender.com/api/persons';
 
 const getAll = () => {
     const request = axios.get(baseUrl);
