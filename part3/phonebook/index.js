241eac4f3a1671bbac70b6771e0ab7dc5de11f83
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-07-03 13:05:49 +0100

    moved front end to render production backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,29 +7,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 const cors = require('cors')
 app.use(cors())
-
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
+const Person = require('./models/person')
 
 app.get('/', (request, response) => {
     console.log(request.body)
@@ -37,15 +15,19 @@ app.get('/', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const people = persons.length
+    Person.find({}).exec(function (err, results) {
+        var count = results.length
+      });
     const date = new Date()
     response.send(
-        "<p>Phonebook has info for " + people + " people</p>" + "<p>" + date + "</p>"
+        "<p>Phonebook has info for " + count + " people</p>" + "<p>" + date + "</p>"
     )
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,30 +1,51 @@
+const mongoose = require('mongoose')
+
 if (process.argv.length<3) {
-    console.log('give password as argument')
+    console.log('Please supply enough arguments! 1: Password, 2: Name, 3: Number')
     process.exit(1)
   }
-  
+
+  if (process.argv.length == 4) {
+    console.log('Invalid amount of arguments!')
+    process.exit(1)
+  }
   const password = process.argv[2]
-  
+
   const url =
-    `mongodb+srv://parmvirgrewals:${password}@cluster0.ujlyixv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-  
+  `mongodb+srv://parmvirgrewals:${password}@cluster0.ujlyixv.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
   mongoose.set('strictQuery',false)
-  
   mongoose.connect(url)
-  
+
   const personSchema = new mongoose.Schema({
     name: String,
     number: String
   })
-  
-  const Person = mongoose.model('Person', noteSchema)
-  
-  const person = new Person({
-    name: "Jason Derulo",
-    number: "07974939282"
-  })
-  
-  person.save().then(result => {
-    console.log('person saved!')
-    mongoose.connection.close()
-  })
+
+  const Person = mongoose.model('Person', personSchema)
+
+
+if (process.argv.length == 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+      })
+}
+
+else if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    
+    const person = new Person({
+      name: name,
+      number: number
+    })
+    
+    person.save().then(result => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+}
