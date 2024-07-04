2641aedf361961b19fe5af48a788f312d81b80e1
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-07-04 14:47:35 +0100

    updated front end for error message if name is less than 3 characters

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -62,15 +62,21 @@ app.delete('/api/persons/:id', (request, response) => {
     })
 })
 
-app.put('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    Person.findOneAndReplace({_id: id}, request.body, {returnDocument:'after'}).then(result => {
-        console.log(result)
-        return response.json(result)
-    })
-})
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+  
+    Person.findByIdAndUpdate(
+      request.params.id, 
+      { name, number },
+      { new: true, runValidators: true, context: 'query' }
+    ) 
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     if(!request.body.name) {
         return response.status(400).json({ 
             error: 'name missing' 
@@ -95,14 +101,31 @@ app.post('/api/persons', (request, response) => {
             });
             person.save().then(savedPerson => {
                 return response.json(savedPerson);
-            }).catch(error => {
-                return response.status(500).json({ error: 'failed to save person' });
-            });
+            }).catch(error => next(error));
         }
-    }).catch(error => {
-        return response.status(500).json({ error: error });
-    });
+    }).catch(error => next(error));
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+      }
+    next(error)
+  }
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
