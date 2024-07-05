010b1324fd745655bfb41581e76afb81b5965092
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-07-05 14:56:45 +0100

    added regex validation to phone number insertion

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -19,6 +19,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
       type: String,
+      minLength: 9,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}[\-]\d/.test(v);
+        }
+      },
+      message: props => `${props.value} is not a valid phone number!`,
       required: true
     }
   })
