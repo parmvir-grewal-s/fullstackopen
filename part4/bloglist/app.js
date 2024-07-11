096dc65f16cd267b191acb15ed68516c779cdf88
Parmvir Grewal <parmvirgrewal@Parmvirs-MacBook-Pro-2.local>
2024-07-11 14:24:54 +0100

    refactored structure to meet good practices

---
+++ part4/bloglist/app.js
@@ -0,0 +1,20 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const url = config.MONGODB_URL
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app;
